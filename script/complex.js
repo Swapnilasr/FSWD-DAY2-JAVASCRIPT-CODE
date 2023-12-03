d84a961e978536ac94ab1e26bb7071480b6f8a84
swapnil <3889Swapnil@gmail.com>
2023-12-03 13:02:57 +0530

    javascript datatypes

---
+++ script/complex.js
@@ -0,0 +1,11 @@
+const person={
+    name:'rushi',
+    age:19,
+    city:'solapur'
+
+}
+console.log(person.name)
+console.log(person['city'])
+
+console.log(typeof person)
+console.log(person)
---
+++ script/script.js
@@ -0,0 +1,31 @@
+var emp1='rushi';
+let emp2='sansakr';
+let emp3='maria';
+var emp4= new String('absc');
+
+console.log('emp1:',emp1);
+console.log(emp2);
+console.log(emp3);
+console.log(emp4);
+
+
+console.log(typeof emp4);
+console.log(typeof emp1);
+
+var count=0.675;
+let count1=1244;
+let empno= new Number(21429147);
+
+console.log('count:',count);
+console.log("count1:",count1);
+console.log("count1:",empno);
+console.log(typeof count);
+console.log(typeof empno);
+
+var eligible="ture";
+let datashow='faluse';
+let data= new Boolean('true')
+console.log("count1:",eligible)
+console.log("count1:",data)
+console.log(typeof datashow)
+console.log(typeof data)
